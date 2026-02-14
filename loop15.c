36374be281123f8ace18454c84f2895c58610675
Sa Ra <sa.lahpai@auw.edu.bd>
2026-02-15 00:10:39 +0600

    Add files via upload

---
+++ loop15.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <stdio.h>
+int main ( )
+{
+    int n=4;
+
+ for (int i=1; i<=n; i++)
+ {
+    for(int j=1;j<=i;j++)
+    {
+        printf("*");
+    }
+    printf("\n");
+ }
+
+}
